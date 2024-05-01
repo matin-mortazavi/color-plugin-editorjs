dc9392b946bbccfa0dbe412903d2201363e41ddd
matin.mortazavi.a <matin.mortazavi.a@gmail.com>
2024-05-01 16:24:31 +0330

    feat (folder && index.js) : icon feature added || fodlers changed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-class ColorTool {
+export default class ColorTool {
   static get isInline() {
     return true;
   }
@@ -20,7 +20,8 @@ class ColorTool {
     this.button.classList.toggle(this.api.styles.inlineToolButtonActive, state);
   }
 
-  constructor({ api }) {
+  constructor({ config, api }) {
+    this.config = config;
     this.api = api;
     this.button = null;
     this._state = false;
@@ -32,8 +33,9 @@ class ColorTool {
   render() {
     this.button = document.createElement("button");
     this.button.type = "button";
-    this.button.innerHTML =
-      '<svg width="20" height="18"><path d="M10.458 12.04l2.919 1.686-.781 1.417-.984-.03-.974 1.687H8.674l1.49-2.583-.508-.775.802-1.401zm.546-.952l3.624-6.327a1.597 1.597 0 0 1 2.182-.59 1.632 1.632 0 0 1 .615 2.201l-3.519 6.391-2.902-1.675zm-7.73 3.467h3.465a1.123 1.123 0 1 1 0 2.247H3.273a1.123 1.123 0 1 1 0-2.247z"/></svg>';
+    this.button.innerHTML = this.config?.icon
+      ? this.config?.icon
+      : `<svg fill="#000000" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M17.6,6L6.9,16.7c-0.2,0.2-0.3,0.4-0.3,0.6L6,23.9c0,0.3,0.1,0.6,0.3,0.8C6.5,24.9,6.7,25,7,25c0,0,0.1,0,0.1,0l6.6-0.6 c0.2,0,0.5-0.1,0.6-0.3L25,13.4L17.6,6z"></path> <path d="M26.4,12l1.4-1.4c1.2-1.2,1.1-3.1-0.1-4.3l-3-3c-0.6-0.6-1.3-0.9-2.2-0.9c-0.8,0-1.6,0.3-2.2,0.9L19,4.6L26.4,12z"></path> </g> <g> <path d="M28,29H4c-0.6,0-1-0.4-1-1s0.4-1,1-1h24c0.6,0,1,0.4,1,1S28.6,29,28,29z"></path> </g> </g></svg>`;
     this.button.classList.add(this.api.styles.inlineToolButton);
 
     return this.button;
@@ -51,8 +53,7 @@ class ColorTool {
   wrap(range) {
     const selectedText = range.extractContents();
     const elem = document.createElement(this.tag);
-    // mark.style.color = "red";
-    console.log(elem);
+
     elem.classList.add(this.class);
     elem.appendChild(selectedText);
     range.insertNode(elem);
@@ -84,7 +85,7 @@ class ColorTool {
   renderActions() {
     this.colorPicker = document.createElement("input");
     this.colorPicker.type = "color";
-    this.colorPicker.value = "#f5f1cc";
+    this.colorPicker.value = this.config?.defaultColor || "#FFF";
     this.colorPicker.hidden = true;
 
     return this.colorPicker;
@@ -120,5 +121,3 @@ class ColorTool {
     return "#" + hexr + hexg + hexb;
   }
 }
-
-export default ColorTool;
